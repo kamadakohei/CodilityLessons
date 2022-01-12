b8a9fa9b52ad21a511453d9e896d67ec534016e2
kamadakohei <kamada.math@gmail.com>
2022-01-12 21:04:22 +0900

    3-2 solved

---
+++ 3-2.py
@@ -0,0 +1,6 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    return sum(range(1,len(A)+2))-sum(A)
